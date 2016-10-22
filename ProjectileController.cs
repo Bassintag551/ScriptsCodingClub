e4d760f0295701f2b6ce2ca119c94ce96bea9cb3
Antoine S <antoine.stempfer@epitech.eu>
2016-10-22 19:21:06 +0200

    Added Projectile controller

--- ProjectileController.cs
+++ ProjectileController.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class ProjectileController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Représente la duréede vie du projectile
+    public float    lifespan = 10f;
+
+    // Représente la vitesse du projectile
+    public Vector2  speed = new Vector2(10f, 0f);
+
+    // Cette fonction se lance à chaque frame
+    void FixedUpdate()
+    {
+        Vector3     relativeSpeed;                                                  // Représente la vitesse du projectile relativement au temps écoulé depuis la dernière frame
+
+        relativeSpeed = speed * Time.deltaTime;                                     // Assigne la valeur de relativeSpeeds
+        transform.position = new Vector3(transform.position.x + relativeSpeed.x,    // Modifie la position du projectile
+                                         transform.position.y + relativeSpeed.y);
+        if (lifespan <= 0)                                                          // Vérifie si l'objet doit diparaitre
+            Destroy(gameObject);                                                    // Détruit l'objet
+        else
+            lifespan -= Time.deltaTime;                                             // Met à jour la durée de vie du projectile
+    }
 }
